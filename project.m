0ff00c125376785a01ed824a8a4d8221a7b36c25
Siavash <siavash.davani@gmail.com>
2018-07-11 21:53:49 +0430

    project upto choosing best features

--- project.m
+++ project.m
@@ -3,21 +3,25 @@
 load('./Data/SubHandData');
 
 %% Extracting Features
-features = featureExtracter(TrainX);
+[features, NUM_OF_FEATURES] = featureExtracter(TrainX);
 mu = mean(features, 2);
 sigma = std(features, 0, 2);
 
 features = normalizer(features, mu, sigma);
 
-%% Choosing Features
+%% Choosing Best Features
 
+J = jComputer(features, Trainy);
 
+[sortedJ, sortedJindex] = sort(J, 'descend');
 
-
-
+% bFeatures = features(sortedJindex(1:3), :);
+bFeatures = features( J > .01 , :);
 
 
 %% Estimating Error
+
+
 
 
 
