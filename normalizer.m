c68e33c85f5cd32a927003488531cca4f704f24e
Siavash <siavash.davani@gmail.com>
2018-07-11 21:52:04 +0430

    fix

--- normalizer.m
+++ normalizer.m
@@ -3,7 +3,7 @@ function [ output ] = normalizer( input, mu, sigma )
   [numOfFeatures, ~] = size(input);
   output = zeros(size(input));
   
-  for i = numOfFeatures
+  for i = 1:numOfFeatures
     
     output(i, :) = (input(i, :) - mu(i))/sigma(i);
     
