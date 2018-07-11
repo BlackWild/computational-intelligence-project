1a4062ca434c4ab5e591804aca42c0d0f479a1b2
Siavash <siavash.davani@gmail.com>
2018-07-11 21:52:43 +0430

    adding feature

--- featureExtracter.m
+++ featureExtracter.m
@@ -1,14 +1,22 @@
-function features = featureExtracter( data )
+function [features, NUM_OF_FEATURES] = featureExtracter( data )
   
-  NUM_OF_FEATURES = 2;
+  NUM_OF_FEATURES = 6;
   [~, ~, numOfExperiments] = size(data);
   
   features = zeros(NUM_OF_FEATURES, numOfExperiments);
   
   for i = 1:numOfExperiments
     
-    features(1, i) = max(abs(data(1, :, i)));
-    features(2, i) = mean(data(1, :, i));
+    [~, t] = max(abs(data(2, :, i)));
+    features(1, i) = t;
+    
+    t = cov(data(2, :, i), data(4, :, i));
+    features(2, i) = t(1,1);
+    features(3, i) = t(2,2);
+    features(4, i) = t(1,2);
+    
+    features(5, i) = mean(mean(data(:, :, i)));
+    features(6, i) = max(var(data(:, :, i)));
     
   end
 
