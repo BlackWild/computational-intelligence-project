a4be2e7bea8e1495eba30444118945b0c23289f0
Siavash <siavash.davani@gmail.com>
2018-07-13 15:49:35 +0430

    adding more features

--- featureExtracter.m
+++ featureExtracter.m
@@ -1,24 +1,62 @@
 function [features, NUM_OF_FEATURES] = featureExtracter( data )
   
-  NUM_OF_FEATURES = 6;
   [~, ~, numOfExperiments] = size(data);
   
-  features = zeros(NUM_OF_FEATURES, numOfExperiments);
-  
-  for i = 1:numOfExperiments
+  features = zeros(1000, numOfExperiments); 
+  c = 0;
+ 
+  for j = 1:30
+
+    % % % Statistical
+
+    c = c + 1 + 1 + 1;
+    for i = 1:numOfExperiments
+      
+      a = data(j, :, i);
+      
+      features(c,   i) = sum( a.^2 );     % Energy
+      
+      [~, t] = max(abs(a));
+      features(c-1, i) = t;               % Peak time
+      
+      features(c-2, i) = max(a) - min(a); % Range
+      
+    end
+
+    for k = j:30
+      c = c + 1;
+      for i = 1: numOfExperiments
+        t = cov(data(j, :, i), data(k, :, i));
+        features(c, i) = t(1,2);     % Covariance + Variance
+      end
+    end
     
-    [~, t] = max(abs(data(2, :, i)));
-    features(1, i) = t;
+
+
+    % Frequency
     
-    t = cov(data(2, :, i), data(4, :, i));
-    features(2, i) = t(1,1);
-    features(3, i) = t(2,2);
-    features(4, i) = t(1,2);
-    
-    features(5, i) = mean(mean(data(:, :, i)));
-    features(6, i) = max(var(data(:, :, i)));
-    
+    c = c + 1+1+1+1+1+1+1;
+    for i = 1:numOfExperiments
+      
+      a = data(j, :, i);
+      
+      features(c,   i) = meanfreq(a, 256);
+      features(c-1, i) = medfreq(a, 256);
+      
+      features(c-2, i) = bandpower(a, 256, [2, 8]);
+      features(c-3, i) = bandpower(a, 256, [8, 15]);
+      features(c-4, i) = bandpower(a, 256, [15, 22]);
+      features(c-5, i) = bandpower(a, 256, [22, 29]);
+      features(c-6, i) = bandpower(a, 256, [29, 36]);
+      
+    end
+
+
   end
+    
+  
+  NUM_OF_FEATURES = c;
+  features = features(1:c, :);
 
 end
 
